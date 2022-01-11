0a605b65392b82b5e65f4cde17b0310de98404fa
Fued <fouadhorri12345@gmail.com>
2022-01-11 15:34:06 +0100

    f

---
+++ src/main/java/com/weather/P1/P1WeatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.P1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1WeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/P1/P1WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.P1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
